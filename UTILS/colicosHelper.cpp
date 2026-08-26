dc61cec49dab0726b85112e249ee633d27a72f16
vakdev <zethus810t5@gmail.com>
2026-08-25 22:42:55 -0600

    colicosHelper.cpp

---
+++ UTILS/colicosHelper.cpp
@@ -0,0 +1,24 @@
+/*
+Hace que se use el 100% del CPU calentando la PC/Laptop.
+
+savar widell
+*/
+
+
+#include <vector>
+#include <thread>
+
+int main() {
+	std::vector<std::thread> z;
+
+	for (unsigned i = 0; i < std::thread::hardware_concurrency();i++) {
+		z.emplace_back([] {
+			volatile double x = 0;
+			while (true) {
+				x += 1.0;
+			}
+			});
+	}
+
+	for (auto& h : z) h.join();
+}
