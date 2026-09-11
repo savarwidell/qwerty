f845081fad5a045806ab3ba054c721ef94f5c2cf
savarwidell <savarwidell@gmail.com>
2026-09-11 16:18:45 -0600

    Update Main.java

--- Ale/OOP-Silvia/Arreglos Dinamicos/Main.java
+++ Ale/OOP-Silvia/Arreglos Dinamicos/Main.java
@@ -6,14 +6,13 @@ public class Main {
         ArrayList<String> productos = new ArrayList<>();
 
         productos.add("Laptop");
-        productos.add("Mouse");      
+        productos.add("Mouse");
         productos.add("Teclado");
-        
+
         System.out.println("Productos del inventario:");
 
         for (int i = 0; i < productos.size(); i++) {
             System.out.println((i + 1) + ". " + productos.get(i));
         }
     }
-
 }
